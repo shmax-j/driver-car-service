769c8f32e81449dc844ee8dc1b029e1d7a0963a4
shmax <mshveda28@gmail.com>
2022-11-17 12:16:51 +0200

    Replace servlet configuration in web.xml with annotations

--- src/main/java/taxi/controller/car/AddDriverToCarController.java
+++ src/main/java/taxi/controller/car/AddDriverToCarController.java
@@ -2,6 +2,7 @@ package taxi.controller.car;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import taxi.model.Driver;
 import taxi.service.CarService;
 import taxi.service.DriverService;
 
+@WebServlet(urlPatterns = "/cars/drivers/add")
 public class AddDriverToCarController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
--- src/main/java/taxi/controller/manufacturer/GetAllManufacturersController.java
+++ src/main/java/taxi/controller/manufacturer/GetAllManufacturersController.java
@@ -3,6 +3,7 @@ package taxi.controller.manufacturer;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import taxi.lib.Injector;
 import taxi.model.Manufacturer;
 import taxi.service.ManufacturerService;
 
+@WebServlet(urlPatterns = "/manufacturers")
 public class GetAllManufacturersController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private final ManufacturerService manufacturerService = (ManufacturerService) injector
