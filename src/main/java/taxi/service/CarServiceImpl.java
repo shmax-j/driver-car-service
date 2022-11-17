805fadcdb5b5f54609b6bfbdac30d8379130dc64
shmax <mshveda28@gmail.com>
2022-11-17 12:57:13 +0200

    Move all duplicated service code to GenericServiceImpl

--- src/main/java/taxi/service/CarServiceImpl.java
+++ src/main/java/taxi/service/CarServiceImpl.java
@@ -2,13 +2,15 @@ package taxi.service;
 
 import java.util.List;
 import taxi.dao.CarDao;
+import taxi.dao.GenericDao;
 import taxi.lib.Inject;
 import taxi.lib.Service;
 import taxi.model.Car;
 import taxi.model.Driver;
 
 @Service
-public class CarServiceImpl implements CarService {
+public class CarServiceImpl extends GenericServiceImpl<Car>
+        implements CarService {
     @Inject
     private CarDao carDao;
 
@@ -30,27 +32,7 @@ public class CarServiceImpl implements CarService {
     }
 
     @Override
-    public Car create(Car car) {
-        return carDao.create(car);
-    }
-
-    @Override
-    public Car get(Long id) {
-        return carDao.get(id).get();
-    }
-
-    @Override
-    public List<Car> getAll() {
-        return carDao.getAll();
-    }
-
-    @Override
-    public Car update(Car car) {
-        return carDao.update(car);
-    }
-
-    @Override
-    public boolean delete(Long id) {
-        return carDao.delete(id);
+    protected GenericDao<Car> getDao() {
+        return carDao;
     }
 }
---
+++ src/main/java/taxi/service/GenericServiceImpl.java
@@ -0,0 +1,34 @@
+package taxi.service;
+
+import java.util.List;
+import taxi.dao.GenericDao;
+
+public abstract class GenericServiceImpl<T> implements GenericService<T>{
+
+    @Override
+    public T create(T element) {
+        return getDao().create(element);
+    }
+
+    @Override
+    public T get(Long id) {
+        return getDao().get(id).get();
+    }
+
+    @Override
+    public List<T> getAll() {
+        return getDao().getAll();
+    }
+
+    @Override
+    public T update(T element) {
+        return getDao().update(element);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return getDao().delete(id);
+    }
+
+    protected abstract GenericDao<T> getDao();
+}
