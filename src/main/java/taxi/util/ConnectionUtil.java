97f672503f32fe881e5bcdd6c351dafcc7785c50
shmax <mshveda28@gmail.com>
2022-11-18 16:40:49 +0200

    Remove unused imports and classes

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -1,10 +1,7 @@
 package taxi.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
